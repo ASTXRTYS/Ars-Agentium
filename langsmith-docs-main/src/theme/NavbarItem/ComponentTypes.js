bf5ba4bc795e77c389c88f1b7e04bcfc61e0adfb
ASTXRTYS <jason_m13@icloud.com>
2025-11-16 16:04:16 -0500

    chore: remove existing langsmith docs snapshot

--- langsmith-docs-main/src/theme/NavbarItem/ComponentTypes.js
+++
@@ -1,7 +0,0 @@
-import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
-import RegionSelector from "../../components/Region";
-
-export default {
-  ...ComponentTypes,
-  "custom-RegionSelector": RegionSelector,
-};
--- langsmith-docs-main/tests/js_unit_tests/tracing/tracing-faq/test_code_block_0.test.ts
+++
@@ -1,6 +0,0 @@
-import { ChatAnthropic } from "langchain/chat_models/anthropic";
-
-test("test_code_block_0", async () => {
-  const llm = new ChatAnthropic();
-  await llm.invoke("Hello, World!");
-});
