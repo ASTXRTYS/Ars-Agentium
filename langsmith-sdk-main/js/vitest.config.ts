fa9e64dfdfff797700c9f6f2855971a940e0dff3
ASTXRTYS <jason_m13@icloud.com>
2025-11-16 16:05:52 -0500

    chore: remove existing langsmith sdk snapshot

--- langsmith-sdk-main/js/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["**/*.vitesttest.?(c|m)[jt]s"],
-    setupFiles: ["dotenv/config"],
-    hookTimeout: 30000,
-  },
-});
